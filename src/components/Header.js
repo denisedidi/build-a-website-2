1800f96d2199bcc23cb8e2abe127e8a26db2c0e4
denise <prasetyo.denise@gmail.com>
2020-12-29 18:07:42 +0700

    removed node_modules, DS_Store, and added router

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ function Header() {
   return (
     <header>
       <div>
-        <h1> TODO </h1>
+        <h1> Denise's weather app </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=Seoul"> Seoul </a>
+        <a href="/?city=London"> London </a>
+        <a href="/?city=Jakarta"> Jakarta </a>
+        <a href="/?city=Bangkok"> Bangkok </a>
       </nav>
     </header>
   );
