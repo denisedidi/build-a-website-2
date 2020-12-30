54fce37e370641c3c162beff3d7a77e5428a2773
denise <prasetyo.denise@gmail.com>
2020-12-30 17:26:35 +0700

    added cities

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,11 +4,19 @@ import React, { useState, useEffect, useMemo } from "react";
 
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
-
+import City from "../components/City";
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
+  return (
+   <div className="flex flex-col h-screen bg-green-200"> 
+      <City cityName="Jakarta" temp="37°C" color="bg-yellow-200"/>
+      <City cityName="Bangkok" temp="35,5°C" color="bg-blue-200"/>
+      <City cityName="Tokyo" temp="26°C" color="bg-pink-200"/>
+    </div>
+
+  );
 }
 
 export default Home;
