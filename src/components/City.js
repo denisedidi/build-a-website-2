0bd5c193430c1d54b38be350ae5cb43ddd1ef904
denise <prasetyo.denise@gmail.com>
2021-01-01 12:39:20 +0700

    added City.js

--- src/components/City.js
+++ src/components/City.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
 
-export default function City({cityName, temp, color}) {
+// props -> cityName ,temp, color
+function City({ cityName, weatherType, temp = 0, color }) {
   const history = useHistory();
 
   function handleClick() {
@@ -13,8 +14,15 @@ export default function City({cityName, temp, color}) {
       className={"flex flex-row p-8 justify-between items-center " + color}
       onClick={handleClick}
     >
-      <div className="text-2xl font-light">{cityName}</div>
-      <div className="text-5xl">{temp}</div>
+      <div className="flex flex-col">
+        <div className="text-md font-light text-left">{weatherType}</div>
+        <div className="text-2xl font-light">{cityName}</div>
+      </div>
+
+      <div className="text-5xl">{Math.round(temp)} °C</div>
     </button>
   );
 }
+
+export default City;
+
